b9fb58c6023e1dde6d6336710bc19c42f810c34f
lenovo <buqian0627@163.com>
2019-03-28 16:08:45 +0800

    代码优化

--- Vue/vue-demo/src/api/services/about.js
+++ Vue/vue-demo/src/api/services/about.js
@@ -4,12 +4,6 @@ export const getBookList = (params = {}) =>
   httpGet({ url: 'goodbook/catalog', params })
   // httpGet({ url: 'joke/content/list.php', params })
 
-// export const save = (data) => {
-//   return httpPost({
-//     url: 'apps/wechat/api/save_member',
-//     data
-//   })
-// }
  export const getWeather = (data) => {
   return httpPost({
     url: '/simpleWeather/query',
--- Vue/vue-demo/src/views/About.vue
+++ Vue/vue-demo/src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="_getWeather">显示天气</button>
+    <ul v-if="weather">
+      <li>城市：{{weather.city}}</li>
+      <li v-for="(item, index) in weather.future" :key="item.id">
+        {{index+1}}:{{item.temperature}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,6 +16,22 @@ import { getBookList, getWeather } from "../api/services/about";
 
 export default {
   name: 'about',
+  data() {
+    return {
+      weather: ''
+    }
+  },
+  methods: {
+    _getWeather () {
+      getWeather({
+        key: '45293d3aebf2ed9fc10a0ae6a365a563',
+        city: '北京'
+      }).then(res => {
+        console.log(res.result)
+        this.weather = res.result
+      })
+    }
+  },
   created() {
     // key=9175b2ed55a7fe19c49a497e8d405b43&dtype=json
     // joke/content/list.php?key=2aeb61008fde5920f66d2492709a2f8e&page=2&pagesize=10&sort=asc&time=1418745237
@@ -19,12 +42,7 @@ export default {
       console.log(res.result)
     })
 
-    getWeather({
-      key: '45293d3aebf2ed9fc10a0ae6a365a563',
-      city: '北京'
-    }).then(res => {
-      console.log(res.result)
-    })
+
     // getBookList({
     //   key:'2aeb61008fde5920f66d2492709a2f8e',
     //   page: 2,

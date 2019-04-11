3b0783d2a40495db95cc3381415448909bb636fe
lenovo <buqian0627@163.com>
2019-04-11 17:14:41 +0800

    axios拦截器

--- Vue/vue-demo/src/api/services/instances.js
+++ Vue/vue-demo/src/api/services/instances.js
@@ -1,5 +1,9 @@
 // http.js
 import axios from 'axios'
+import router from '../../router'
+import {MessageBox, Message} from 'element-ui'
+
+let loginUrl = '/login'
 
 // 环境的切换
 if (process.env.NODE_ENV === 'development') {
@@ -7,6 +11,8 @@ if (process.env.NODE_ENV === 'development') {
 } else if (process.env.NODE_ENV === 'production') {
   axios.defaults.baseURL = 'http://apis.juhe.cn'
 }
+axios.defaults.headers = {'X-Requested-With': 'XMLHttpRequest'}
+axios.defaults.timeout = 60000
 
 // 请求拦截器
 // axios.interceptors.request.use(
@@ -17,24 +23,74 @@ if (process.env.NODE_ENV === 'development') {
 //   error => {
 //     return Promise.error(error)
 //   })
+axios.interceptors.request.use(
+  config => {
+    if (router.history.current.path !== loginUrl) {
+      let token = window.sessionStorage.getItem('token')
+      if (token == null) {
+        router.replace({path: loginUrl, query: {redirect: router.currentRoute.fullPath}})
+        return false
+      } else {
+        config.headers['Authorization'] = 'JWT ' + token
+      }
+    }
+    return config
+  }, error => {
+    Message.warning(error)
+    return Promise.reject(error)
+  })
+
 
 // axios.defaults.timeout = 10000
 
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 
 // 响应拦截器
-// axios.interceptors.response.use(response => {
-//   if (response.status === 200) {
-//     return Promise.resolve(response)
-//   } else {
-//     return Promise.reject(response)
-//   }
-// }, error => {
-//   if (error.response.status) {
-//     // 对不同返回码对相应处理
-//     return Promise.reject(error.response)
-//   }
-// })
+axios.interceptors.response.use(
+  response => {
+    return response.data
+  }, error => {
+    if (error.response !== undefined) {
+      switch (error.response.status) {
+        case 400:
+          MessageBox.alert(error.response.data)
+          break
+        case 401:
+          if (window.sessionStorage.getItem('out') === null) {
+            window.sessionStorage.setItem('out', 1)
+            MessageBox.confirm('会话已失效! 请重新登录', '提示', {
+              confirmButtonText: '重新登录',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              router.replace({path: loginUrl, query: {redirect: router.currentRoute.fullPath}})
+            }).catch(action => {
+              window.sessionStorage.clear()
+              window.localStorage.clear()
+            })
+          }
+          break
+        case 402:
+          MessageBox.confirm('登陆超时 ！', '提示', {
+            confirmButtonText: '重新登录',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(() => {
+            router.replace({path: loginUrl, query: {redirect: router.currentRoute.fullPath}})
+          })
+          break
+        case 403:
+          MessageBox.alert('没有权限！')
+          break
+        // ...忽略
+        default:
+          MessageBox.alert(`连接错误${error.response.status}`)
+      }
+      return Promise.resolve(error.response)
+    }
+    return Promise.resolve(error)
+  })
+
 
 // get 请求
 export function httpGet({

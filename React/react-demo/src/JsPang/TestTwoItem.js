5e6e639e8fafee98a8487887b53cba14ece1ef4f
buqian <buqian0627@163.com>
2019-07-04 08:16:40 +0800

    prop-types

--- React/react-demo/src/JsPang/TestTwoItem.js
+++ React/react-demo/src/JsPang/TestTwoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 
 class TestTwoItem extends Component {
 
@@ -19,5 +20,11 @@ class TestTwoItem extends Component {
     )
   }
 }
+
+TestTwoItem.propTypes = {
+  content: PropTypes.string
+}
+
+
 
 export default TestTwoItem

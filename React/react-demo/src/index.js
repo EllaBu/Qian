853dc34c99b29d32ef77255b86f8926d747a0f47
lenovo <buqian0627@163.com>
2019-07-11 10:30:42 +0800

    redux--安装antd编写界面样式

--- React/react-demo/src/index.js
+++ React/react-demo/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TestTwo from './JsPang/TestTwo';
+import TestTwo from './Todo/TodoList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<TestTwo />, document.getElementById('root'));

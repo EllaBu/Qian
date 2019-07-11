eb9549f855f5021ee2f46f5d6ca369cd716573bd
lenovo <buqian0627@163.com>
2019-07-11 17:07:45 +0800

    redux--特别注意

--- React/react-demo/src/srore/index.js
+++ React/react-demo/src/srore/index.js
@@ -4,3 +4,9 @@ const store = createStore(reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())          // 创建数据存储仓库
 
 export default store
+
+
+
+// store必须是唯一的，多个store是坚决不允许，只能有一个store空间
+// 只有store能改变自己的内容，Reducer不能改变
+// Reducer必须是纯函数

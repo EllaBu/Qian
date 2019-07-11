9a188bb2c084a34d961a7823df86a322e2349cf9
lenovo <buqian0627@163.com>
2019-07-11 16:45:00 +0800

    redux--使用redux改写TodoList

---
+++ React/react-demo/src/Todo/TodoList.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+import store from '../srore'
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    /*this.state = {
+      data: [
+        '早8点开晨会，分配今天的开发工作',
+        '早9点和项目经理作开发需求讨论会',
+        '晚5:30对今日代码进行review'
+      ]
+    }*/
+    this.state = store.getState()
+    console.log(store.getState())
+
+    this.inputChange = this.inputChange.bind(this)
+    this.storeChange = this.storeChange.bind(this)
+    this.deleteItem = this.deleteItem.bind(this)
+
+    store.subscribe(this.storeChange)  // 订阅Redux的状态
+  }
+
+  inputChange(e) {
+    /*this.setState({
+      inputValue: e.target.value
+    })*/
+    const action = {
+      type: 'changeInput',
+      value: e.target.value
+    }
+    store.dispatch(action)
+
+    console.log(this.state.inputValue)
+  //  创建action
+  }
+
+  storeChange() {
+    this.setState(
+      store.getState()
+    )
+  }
+
+  addItem() {
+    /*this.setState({
+      list: [...this.state.list, this.state.inputValue]
+    })*/
+    const action = { type: 'addItem' }
+    store.dispatch(action)
+  }
+
+  deleteItem (index) {
+    console.log(index)
+    const action = {
+      type: 'deleteItem',
+      index
+    }
+    store.dispatch(action)
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <header style={{margin: '10px'}}>
+          <Input
+            placeholder={this.state.inputValue}
+            style={{width: '250px', marginRight: '10px'}}
+            onChange={this.inputChange}
+          />
+          <Button
+            type='primary'
+            onClick={this.addItem}
+          >增加</Button>
+        </header>
+
+        <div style={{margin:'10px',width:'300px'}}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={(item, index)=>(
+              <List.Item
+                onClick={this.deleteItem.bind(this, index)}
+              >{item}</List.Item>
+            )}
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
+export default TodoList
---
+++ React/react-demo/src/srore/reducer.js
@@ -0,0 +1,41 @@
+const defaultState = {
+  inputValue: 'Write Something',
+  list: [
+    '早上4点起床，锻炼身体',
+    '中午下班游泳一小时'
+  ]
+}   // 给Store里增加了两个新的数据。
+
+// state: 是整个项目中需要管理的数据信息
+
+
+export default ( state = defaultState, action ) => {
+
+  console.log(state,action)
+
+  // state: 指的是原始仓库里的状态。
+  // action: 指的是action新传递的状态。
+
+  if(action.type === 'changeInput') {
+    // Reducer里只能接收state，不能改变state
+    let newState = JSON.parse(JSON.stringify(state)) // 深度拷贝state
+    newState.inputValue = action.value
+    return newState
+  }
+
+  if(action.type === 'addItem') {
+    let newState = JSON.parse(JSON.stringify(state)) // 深度拷贝state
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+
+  if(action.type === 'deleteItem') {
+    let newState = JSON.parse(JSON.stringify(state)) // 深度拷贝state
+    newState.list.splice(action.index, 1)
+    return newState
+  }
+
+
+  return state
+}
